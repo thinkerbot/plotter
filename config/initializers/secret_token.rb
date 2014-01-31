f15c78e701d23414cdf345b6bd6ba0d08ae6a2ab
Simon Chiang <simon.a.chiang@gmail.com>
2014-01-30 20:37:48 -0700

    Basic setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Plotter::Application.config.secret_key_base = 'ff22ad5e393c2b1f92c191e5fff6c345492989e902976a7e5d6a81e7df2eff274ad294d7089f45b307ed37d1d1342956e63ff1233271d5e2a08eae7e9efa2805'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Plotter::Application.config.session_store :cookie_store, key: '_plotter_session'
